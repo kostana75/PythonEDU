724bbd514d62e6368cde34439152ec51bfe64fca
Татьяна Косарева <kosareva_tana@mail.ru>
2024-08-08 16:04:17 +0300

    Урок5_Задание3.py

---
+++ PythonApplication1/Урок5_Задание3.py
@@ -0,0 +1,13 @@
+﻿summa = int(input('Минимальная сумма инвестиций: '))
+mike = int(input('Количество долларов у Майкла: '))
+ivan = int(input('Количество долларов у Ивана: '))
+if (mike >= summa) and (ivan >= summa):
+  print(2)
+elif (mike >= summa) and (ivan <= summa):
+  print('Mike')
+elif (mike <= summa) and (ivan >= summa):
+  print("Ivan")
+elif (mike <= summa) and (ivan <= summa) and ((mike + ivan) >= summa):
+  print(1)
+elif (mike <= summa) and (ivan <= summa) and ((mike + ivan) <= summa):
+  print(0)
