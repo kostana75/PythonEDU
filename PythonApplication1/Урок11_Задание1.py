c77402a89315b56994b9f4c2c763618fa08545c4
Татьяна Косарева <kosareva_tana@mail.ru>
2024-08-09 22:52:49 +0300

    Урок11_Задание1.py

---
+++ PythonApplication1/Урок11_Задание1.py
@@ -0,0 +1,10 @@
+﻿def fac(n):
+  if n == 0:
+    return 1
+  return fac(n-1) * n
+
+sp = []
+y = int(input('Введите натуральное целое число: '))
+for i in range(y, 0, -1):
+  sp.append(fac(i))
+print(sp)
