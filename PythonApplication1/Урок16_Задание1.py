023ceb1ecff607220c2b2a94555275c280a07878
Татьяна Косарева <kosareva_tana@mail.ru>
2024-08-10 23:30:03 +0300

    Урок16_Задание1.py

---
+++ PythonApplication1/Урок16_Задание1.py
@@ -0,0 +1,18 @@
+﻿class Cassa:
+  summa = 25125 # количество денег в кассе
+  def top_up(self, pokup):
+    self.pokup = pokup
+    pokup += Cassa.summa
+    return f"в кассе {pokup}"
+
+  def count_1000(self):
+    print(Cassa.summa // 1000)
+
+  def take_away(self, x):
+    if x <= self.summa:
+      self.summa -= x
+    else:
+      return f"не достаточно денег"
+
+r=Cassa()
+print(r.top_up(125))
